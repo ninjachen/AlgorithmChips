1dc1c46698d221e9cf24c3000fb2c43ecc0e6626
jiachenning <jiachenning@gmail.com>
2014-05-14 00:45:20 +0800

    debuting quicksort

--- Algorithm/src/me/ninja/algorithm/Sort.java
+++ Algorithm/src/me/ninja/algorithm/Sort.java
@@ -5,16 +5,21 @@ package me.ninja.algorithm;
  */
 public class Sort {
 
+    private static int swapCount = 0;
     public static void main(String[] args) {
 
         //init test data
-        int [] unsorted = new int[100];
+//        int [] unsorted = {1,2,3,5,4,2,6,2,1,1};
+        int [] unsorted = new int[10];
         for (int i  = 0; i < unsorted.length; i++){
           unsorted[i] = (int)(Math.random() * 1000);
         }
+        System.out.println("交换结束前");
+        show(unsorted);
 
-        quickSort(unsorted, 0 , unsorted.length -1);
+        quickSort1(unsorted, 0 , unsorted.length -1);
 
+        System.out.println("交换结束后");
         show(unsorted);
     }
 
@@ -27,33 +32,72 @@ public class Sort {
      * @param right right index
      */
     public static void quickSort(int[] unsorted, int left, int right ){
-        int middle = (left + right) / 2;
-        int compareValue = unsorted[middle];
         //error check
         if(left > right) return;
+        int middle = (left + right) / 2;
+        int compareValue = unsorted[middle];
         int leftPointer = left;
         int rightPointer = right;
         while (leftPointer < rightPointer){
-            while (unsorted[leftPointer++] < compareValue) {
-                if(leftPointer == right) break;
+            while (unsorted[leftPointer] < compareValue) {
+                leftPointer++;
             }
-            while (unsorted[rightPointer--] > compareValue) {
-                if(rightPointer == left) break;
+            while (unsorted[rightPointer] > compareValue) {
+                rightPointer--;
             }
             if(leftPointer < rightPointer){
             swap(unsorted, leftPointer, rightPointer);
-            leftPointer++;
             rightPointer--;
+            leftPointer++;
             }
         }
-        quickSort(unsorted, left, middle - 1);
-        quickSort(unsorted, middle + 1, right);
+        if(left < leftPointer -1){
+            quickSort(unsorted, left, leftPointer - 1);
+        }
+        if(leftPointer < right)
+            quickSort(unsorted, leftPointer, right);
+    }
+
+    public static void quickSort1(int[] unsorted, int left, int right ){
+        //error check
+//
+//        int leftPointer = left;
+//        int rightPointer = right;
+        int leftPointer = partition(unsorted, left, right);
+        if(left < leftPointer -1){
+            quickSort(unsorted, left, leftPointer - 1);
+        }
+        if(leftPointer < right)
+            quickSort(unsorted, leftPointer, right);
+    }
+
+    public static int partition(int[] unsorted, int leftPointer, int rightPointer){
+        int middle = (leftPointer + rightPointer) / 2;
+        int compareValue = unsorted[middle];
+        while (leftPointer < rightPointer){
+            while (unsorted[leftPointer] < compareValue) {
+                leftPointer++;
+            }
+            while (unsorted[rightPointer] > compareValue) {
+                rightPointer--;
+            }
+            if(leftPointer < rightPointer){
+                swap(unsorted, leftPointer, rightPointer);
+                rightPointer--;
+                leftPointer++;
+            }
+        }
+        return leftPointer;
     }
 
     public static void swap(int[] unsorted, int i, int j){
+        swapCount++;
+        System.out.println("第" + swapCount + "次交换"+i+","+j+":");
         int temp = unsorted[i];
         unsorted[i] = unsorted[j];
         unsorted[j] = temp;
+
+        show(unsorted);
     }
 
     public static void show(int[] data){
